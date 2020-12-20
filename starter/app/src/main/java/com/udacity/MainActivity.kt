45878c63e2b00acb9478465291430bbad3110f1d
Charles Johnson <35396909+ThaDocsN@users.noreply.github.com>
2020-12-20 12:52:43 -0600

    notifications are functional

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityMainBinding
 import com.udacity.databinding.ContentMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
 
+    private val NOTIFICATION_ID = 0
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
@@ -40,6 +45,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.contentView.customButton.setOnClickListener {
             getUrl()
+        }
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        // Check to see if the API Level is a API Level 26 as it requires a channel to be created
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Download is complete!"
+
+            notificationManager.createNotificationChannel(notificationChannel)
         }
     }
 
@@ -60,10 +78,15 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            notificationManager.sendNotification("Test", applicationContext, "Complete")
         }
     }
 
     private fun download(url:String) {
+
+        notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        createChannel(getString(R.string.download_notification_channel_id), getString(R.string.download_notification_channel_name))
+
         val request =
             DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
@@ -71,7 +94,7 @@ class MainActivity : AppCompatActivity() {
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
+               // .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
@@ -85,4 +108,20 @@ class MainActivity : AppCompatActivity() {
         private const val CHANNEL_ID = "channelId"
     }
 
+
+    fun NotificationManager.sendNotification(messageBody: String, appContext:Context, status:String){
+
+        val intent = Intent(appContext, DetailActivity::class.java)
+        intent.putExtra("fileName", messageBody)
+        intent.putExtra("status", status)
+        pendingIntent = PendingIntent.getActivity(appContext, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(appContext, appContext.getString(R.string.download_notification_channel_id))
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(appContext.getString(R.string.notification_title))
+            .setContentText(messageBody)
+            .addAction(R.drawable.ic_baseline_play_arrow_24,"View", pendingIntent)
+
+        notify(NOTIFICATION_ID, builder.build())
+    }
 }
