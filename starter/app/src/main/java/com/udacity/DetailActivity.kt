33233247a2164d67db889e8d2626d0f2bec3a726
Charles Johnson <35396909+ThaDocsN@users.noreply.github.com>
2020-12-21 01:07:28 -0600

    motion layout functional

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,32 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private val fileName by lazy { intent.getStringExtra("fileName").toString() }
+    private val status by lazy { intent.getStringExtra("status").toString() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        tv_fileName.text = fileName
+        tv_status.text   = status
+
+        btn_main.setOnClickListener {
+            returnToMainActivity()
+        }
+    }
+
+    fun returnToMainActivity() {
+        val  main = Intent(this, MainActivity::class.java)
+        startActivity(main)
     }
 
 }
